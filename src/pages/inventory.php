c3bf9e78b9b3fbaa2428ad3da03a0821bc5c07ae
“hope-rogan” <rogan.hope@gmail.com>
2024-04-19 13:36:40 -0400

    added new navbar coloring

---
+++ src/pages/inventory.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <title>Main Page</title>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+    <div class="page-container">
+        <?php include '../components/navbar.php'; ?> <!-- Include the navbar -->
+        <div class="page-content-container temp">
+
+     
+
+        
+        
+        </div>
+    </div>
+    
+</body>
+
+</html>
