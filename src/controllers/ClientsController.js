383b7d1e4122ef640a9cfeec63b7e69ade027332
Shuo Yang <shuoy.ny@gmail.com>
2024-04-29 11:14:38 -0400

    fill up all the fields in clients table

--- src/controllers/ClientsController.js
+++ src/controllers/ClientsController.js
@@ -6,22 +6,86 @@ const clientsModel = new ClientsModel(
 
 //including sales model
 const SalesModel = require("../models/SalesModel");
-
 const salesModel = new SalesModel(
   "mongodb+srv://cs120:hleIcqccff99VSJc@cluster0.bmluvqb.mongodb.net/"
 );
 
+//including inventory model
+const InventoryModel = require("../models/InventoryModel");
+const inventoryModel = new InventoryModel(
+  "mongodb+srv://cs120:hleIcqccff99VSJc@cluster0.bmluvqb.mongodb.net/"
+);
+
+// async function getClients(req, res) {
+//   try {
+//     const clientsData = await clientsModel.getAllClients();
+
+//     const totalClients = await clientsModel.getTotalNumberOfClients();
+
+//     res.render("clients/clients", {
+//       pageTitle: "Client Data",
+//       customCSS: "/css/clients.css",
+//       totalClients: totalClients,
+//       clients: clientsData,
+//     });
+//   } catch (error) {
+//     console.error("Error accessing client data:", error);
+//     res.status(500).send("Unable to retrieve client data.");
+//   }
+// }
 async function getClients(req, res) {
   try {
     const clientsData = await clientsModel.getAllClients();
-
     const totalClients = await clientsModel.getTotalNumberOfClients();
+
+    // Assuming salesModel and inventoryModel are available and properly initialized
+    const clientIds = clientsData.map((client) => client._id.toString());
+    const salesData = await salesModel.getSalesByClientIds(clientIds);
+    const saleIds = salesData.map((sale) => sale._id);
+    const inventoryData = await inventoryModel.getScooterDetailsBySaleIds(
+      saleIds
+    );
+    // console.log(clientIds);
+    // console.log(salesData);
+    // console.log(saleIds);
+    // console.log(inventoryData);
+    // Integrate sales and inventory data with client data
+    // const clientsWithDetails = clientsData.map((client) => {
+    //   const clientSales = salesData.filter((sale) =>
+    //     sale.client_id.equals(client._id.toString())
+    //   );
+    //   const clientInventory = inventoryData.filter((item) =>
+    //     clientSales.some((sale) => sale._id.equals(item.saleId))
+    //   );
+    const clientsWithDetails = clientsData.map((client) => {
+      const clientSales = salesData.filter(
+        (sale) => sale.client_id === client._id.toString()
+      );
+      const clientInventory = inventoryData.filter((item) =>
+        clientSales.some(
+          (sale) => item.sale_id.toString() === sale._id.toString()
+        )
+      );
+      return {
+        ...client,
+        scooter:
+          clientInventory
+            .map((item) => `${item.color} ${item.make} ${item.model}`)
+            .join(", ") || "N/A",
+        order_status: clientSales.some((sale) => sale.date_completed === null)
+          ? "In Progress"
+          : "No Order",
+        client_status: clientSales.some((sale) => sale.date_completed === null)
+          ? "Active"
+          : "Inactive",
+      };
+    });
 
     res.render("clients/clients", {
       pageTitle: "Client Data",
       customCSS: "/css/clients.css",
       totalClients: totalClients,
-      clients: clientsData,
+      clients: clientsWithDetails, // Pass the modified clients array with additional details
     });
   } catch (error) {
     console.error("Error accessing client data:", error);
--- src/models/SalesModel.js
+++ src/models/SalesModel.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require("mongodb");
-const { ObjectId } = require('mongodb');
-
+const { ObjectId } = require("mongodb");
 
 class SalesModel {
   constructor(uri) {
@@ -43,19 +42,19 @@ class SalesModel {
     const result = await this.sales.insertOne(saleData);
     console.log("Sale created with ID:", result.insertedId);
     await this.client.close();
-    return { _id: result.insertedId }
+    return { _id: result.insertedId };
   }
 
-    // updated sale_id filter to _id
-    async getSale(saleID) {
-      try {
-        await this.connect();
-        const sale = await this.sales.findOne({ _id: new ObjectId(saleID) });
-        return sale;
-      } finally {
-        await this.client.close();
-      }
+  // updated sale_id filter to _id
+  async getSale(saleID) {
+    try {
+      await this.connect();
+      const sale = await this.sales.findOne({ _id: new ObjectId(saleID) });
+      return sale;
+    } finally {
+      await this.client.close();
     }
+  }
 
   //Shuo added get sales by client ID function
 
@@ -74,6 +73,17 @@ class SalesModel {
     await this.connect();
     const totalSales = await this.sales.countDocuments();
     return totalSales;
+  }
+
+  async getSalesByClientIds(clientIds) {
+    // console.log("Client IDs:", clientIds);
+    await this.connect();
+
+    const salesData = await this.sales
+      .find({ client_id: { $in: clientIds } })
+      .toArray();
+    // console.log("Sales data retrieved:", salesData);
+    return salesData;
   }
 }
 
